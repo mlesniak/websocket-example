9a3b3019297c06bd3cd8053fe07e8241e6a0faa9
Michael Lesniak <mlesniak@micromata.de>
2015-07-25 23:53:40 +0200

    Initial commit.

---
+++ src/main/java/com/mlesniak/examples/Main.java
@@ -0,0 +1,12 @@
+package com.mlesniak.examples;
+
+/**
+ * Websocket example.
+ *
+ * @author Michael Lesniak (mlesniak@micromata.de)
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
